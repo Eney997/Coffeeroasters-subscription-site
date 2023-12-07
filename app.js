a0618d23a3905e17336b993a9b939da2a7cd4fc4
Tornike Dzidziguri <tornikedzidziguri2001@gmail.com>
2023-12-07 17:53:38 +0400

    coffeeee

---
+++ app.js
@@ -0,0 +1,23 @@
+const hamburger = document.querySelector('.hamburger')
+const navMenu = document.querySelector('.nav-menu')
+const barIcon = document.querySelector('.bar') 
+
+hamburger.addEventListener('click', () =>{
+    hamburger.classList.toggle('active')
+    navMenu.classList.toggle('active')
+})
+
+document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click',()=>{
+    hamburger.classList.remove('active')
+    navMenu.classList.remove('active')
+}))
+
+
+barIcon.onclick = function(){
+    document.body.classList.toggle('bar')
+    if(document.body.classList.contains('bar')){
+        barIcon.src = "/assets/BurgerMenuXicon.svg"
+    } else {
+        barIcon.src= "/assets/BurgerMenuIcon.svg"
+    }
+}
