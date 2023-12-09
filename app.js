1dbe05e60550eebf74e81669ea973bd57506bce7
Tornike Dzidziguri <tornikedzidziguri2001@gmail.com>
2023-12-09 20:24:20 +0400

    first:page:done

--- app.js
+++ app.js
@@ -1,11 +1,47 @@
 const hamburger = document.querySelector('.hamburger')
 const navMenu = document.querySelector('.nav-menu')
 const barIcon = document.querySelector('.bar') 
+const openPopup = document.querySelectorAll('[data-modal-target]')
+const closePopup = document.querySelectorAll('[data-close-button]')
+const blur = document.getElementById('blur')
+
+
+openPopup.forEach(button => {
+    button.addEventListener('click', () => {
+        const modalTarget = button.getAttribute('data-modal-target');
+        const popup = document.querySelector(modalTarget);
+        openmodals(popup);
+    });
+});
+
+
+closePopup.forEach(button => {
+    button.addEventListener('click', ()=>{
+        const popup = button.closest('.popup')
+        closemodals(popup)
+    })
+})
+
+
+function openmodals(popup) {
+    if(popup == null) return
+    popup.classList.add('active')
+    blur.classList.add('active')
+}
+
+
+function closemodals(popup) {
+    if(popup == null) return
+    popup.classList.remove('active')
+    blur.classList.remove('active')
+}
+
 
 hamburger.addEventListener('click', () =>{
     hamburger.classList.toggle('active')
     navMenu.classList.toggle('active')
 })
+
 
 document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click',()=>{
     hamburger.classList.remove('active')
